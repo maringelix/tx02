d1c599569e1c98dfb7be8beda520e87ec6dc751d
maringelix <maringelix@gmail.com>
2025-12-16 16:30:20 -0300

    fix: Simplify backend and add ARM_USE_MSI/OIDC=false

--- terraform/prd/versions.tf
+++ terraform/prd/versions.tf
@@ -14,9 +14,6 @@ terraform {
     storage_account_name = "tfstatetx02"
     container_name       = "tfstate"
     key                  = "tx02-prd.tfstate"
-    subscription_id      = "a9705497-3374-423a-96d1-1661267148ea"
-    tenant_id            = "398f799c-e9db-4bfc-bc6e-b60fcd7bd1f3"
-    use_azuread_auth     = true
   }
 }
 
