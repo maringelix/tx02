d9e5b19476637ba7a33b9beb14dd8f605d4cdac2
maringelix <maringelix@users.noreply.github.com>
2025-12-17 16:42:22 -0300

    fix: Copy provider.tf to prd directory

---
+++ terraform/prd/provider.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstatetx02"
+    container_name       = "tfstate"
+    key                  = "tx02-prd.tfstate"
+    use_cli              = false
+    use_msi              = false
+    use_oidc             = false
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+
+  # Use Service Principal authentication via environment variables
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID
+  skip_provider_registration = true
+  use_cli                    = false
+  use_msi                    = false
+  use_oidc                   = false
+}
+
+provider "azuread" {}
